559a028eb07756065010414290d1ab4d1bfee3eb
Rachel Goldfarb <rachelgoldfarb@Rachels-MacBook-Pro-2.local>
2016-06-19 13:41:53 -0400

    Rachel finished the exercise

--- story.rb
+++ story.rb
@@ -6,12 +6,25 @@ require 'pry'
 
 #show the user a message that says "Welcome to Movie Rating"
 
+puts("Welcome to Movie Rating")
+
 #show the user a message asking them for the title of a movie
 
+puts("Please input the title of a movie.")
 #store the input in a variable
+
+movie_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of ten
 
+puts("What do you rate that movie out of ten?")
+
 # store the input in a variable
 
-display_story(user_title, user_rating)
+movie_rating = gets.strip
+
+def display_story(user_title, user_rating)
+	puts("#{user_title} is ranked #{user_rating} out of 10!")
+end
+
+display_story(movie_title, movie_rating)
